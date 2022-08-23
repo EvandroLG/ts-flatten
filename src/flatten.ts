ef925c51474bf3e96bdcfc4dc363cb2c7cf946db
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-08-23 17:53:47 +0200

    simplifies flatten code

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,33 +1,35 @@
 import { isPrimitive } from './utils';
 
-function flatten<T>(items: T) {
+function flatten(items: unknown) {
   if (isPrimitive(items) || typeof items === 'function') {
     return items;
   }
 
-  const keys = Object.keys(items);
-  const isArray = Array.isArray(items);
-  let output = isArray ? [] : {};
-
-  for (const key of keys) {
-    const item = items[key];
-
-    if (isArray) {
-      if (Array.isArray(item)) {
-        (output as unknown[]).push(...(flatten(item) as unknown[]));
-      } else {
-        (output as unknown[]).push(flatten(item) as unknown);
-      }
-    } else {
-      if (isPrimitive(item) || Array.isArray(item)) {
-        (output as { [key: string]: unknown })[key] = flatten(item);
-      } else {
-        output = { ...output, ...flatten(item) };
-      }
-    }
+  if (Array.isArray(items)) {
+    return flatArray(items);
   }
 
-  return output;
+  return flatObject(items as { [key: string]: unknown });
+}
+
+function flatArray(items: unknown[]) {
+  return items.reduce((acc: unknown[], item) => acc.concat(flatten(item)), []);
+}
+
+function flatObject(items: { [key: string]: unknown }) {
+  const keys = Object.keys(items);
+
+  return keys.reduce((acc, key) => {
+    const item = items[key];
+
+    if (isPrimitive(item) || Array.isArray(item)) {
+      acc[key] = flatten(item);
+    } else {
+      acc = { ...acc, ...flatten(item) };
+    }
+
+    return acc;
+  }, {});
 }
 
 export default flatten;
